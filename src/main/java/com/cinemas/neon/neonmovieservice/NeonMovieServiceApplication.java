a7afea38c529c4ce42e7e8afdf43ad480dbd222c
Julian Henao Marin <julianhm9612@gmail.com>
2024-05-11 23:47:53 -0500

    Add cors config

--- src/main/java/com/cinemas/neon/neonmovieservice/NeonMovieServiceApplication.java
+++ src/main/java/com/cinemas/neon/neonmovieservice/NeonMovieServiceApplication.java
@@ -2,6 +2,9 @@ package com.cinemas.neon.neonmovieservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class NeonMovieServiceApplication {
@@ -9,5 +12,16 @@ public class NeonMovieServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(NeonMovieServiceApplication.class, args);
 	}
+
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH", "OPTIONS");
+            }
+        };
+    }
 
 }

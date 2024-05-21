61c1c504785162b1d5f7f375ae0d61378a0be4a8
Julian Henao Marin <julianhm9612@gmail.com>
2024-05-20 22:14:41 -0500

    Allow public paths

--- src/main/java/com/cinemas/neon/neonmovieservice/config/SecurityConfig.java
+++ src/main/java/com/cinemas/neon/neonmovieservice/config/SecurityConfig.java
@@ -15,7 +15,8 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
+        http
+            .authorizeHttpRequests(authorize -> authorize.requestMatchers("/movies/**", "/shows/**").permitAll().anyRequest().authenticated())
                 .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
         return http.build();
     }
